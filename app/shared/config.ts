04615d5815ab1c11fd151cff06339a990be9203c
surjits <surjits@vmware.com>
2016-12-14 23:02:37 -0800

    Integrate dashboard  with http://localhost:3000/api/articles (https://www.pivotaltracker.com/story/show/136093103)

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,5 +1,5 @@
 export class Config {
   static apiUrl = "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/";
-  static apiUrlDashboard = "/testdata/dashboard.json";
+  static apiUrlDashboard = "http://localhost:3000/api/articles";
   static token = "";
 }
